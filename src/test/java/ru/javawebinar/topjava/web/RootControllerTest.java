9ef10a5f39842243003ce52d8cd749b44303fa62
Kyrylo Gorbunov <kyryl1504@gmail.com>
2023-12-28 08:55:24 +0100

    HW_10 Binding ajax Meal Controller and fix tests.

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,15 +1,13 @@
 package ru.javawebinar.topjava.web;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.MealTestData.meals;
 import static ru.javawebinar.topjava.TestUtil.userAuth;
 import static ru.javawebinar.topjava.UserTestData.admin;
-import static ru.javawebinar.topjava.util.MealsUtil.getTos;
+import static ru.javawebinar.topjava.UserTestData.user;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -32,7 +30,6 @@ class RootControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    @Disabled
     void getMeals() throws Exception {
         perform(get("/meals"))
                 .andDo(print())
